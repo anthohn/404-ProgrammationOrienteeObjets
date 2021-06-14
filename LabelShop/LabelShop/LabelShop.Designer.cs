967429db96c5f6f2a1c803b6fc86fa66198bbea6
Anthony Höhn <anthonyhohn@gmail.com>
2021-06-14 11:14:32 +0200

    initial commit

---
+++ LabelShop/LabelShop/LabelShop.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace LabelShop
+{
+    partial class LabelShop
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtColorChoice = new System.Windows.Forms.Label();
+            this.ComboBoxColor = new System.Windows.Forms.ComboBox();
+            this.BtnNewGrid = new System.Windows.Forms.Button();
+            this.pnlContainer = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // TxtColorChoice
+            // 
+            this.TxtColorChoice.AutoSize = true;
+            this.TxtColorChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtColorChoice.Location = new System.Drawing.Point(30, 27);
+            this.TxtColorChoice.Name = "TxtColorChoice";
+            this.TxtColorChoice.Size = new System.Drawing.Size(127, 16);
+            this.TxtColorChoice.TabIndex = 0;
+            this.TxtColorChoice.Text = "Choix des couleurs :";
+            // 
+            // ComboBoxColor
+            // 
+            this.ComboBoxColor.FormattingEnabled = true;
+            this.ComboBoxColor.Location = new System.Drawing.Point(33, 62);
+            this.ComboBoxColor.Name = "ComboBoxColor";
+            this.ComboBoxColor.Size = new System.Drawing.Size(124, 21);
+            this.ComboBoxColor.TabIndex = 1;
+            this.ComboBoxColor.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // BtnNewGrid
+            // 
+            this.BtnNewGrid.Location = new System.Drawing.Point(33, 286);
+            this.BtnNewGrid.Name = "BtnNewGrid";
+            this.BtnNewGrid.Size = new System.Drawing.Size(124, 36);
+            this.BtnNewGrid.TabIndex = 2;
+            this.BtnNewGrid.Text = "Nouvelle Grille";
+            this.BtnNewGrid.UseVisualStyleBackColor = true;
+            this.BtnNewGrid.Click += new System.EventHandler(this.BtnNewGrid_Click);
+            // 
+            // pnlContainer
+            // 
+            this.pnlContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlContainer.Location = new System.Drawing.Point(186, 12);
+            this.pnlContainer.Name = "pnlContainer";
+            this.pnlContainer.Size = new System.Drawing.Size(320, 320);
+            this.pnlContainer.TabIndex = 3;
+            this.pnlContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlContainer_Paint);
+            // 
+            // LabelShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 344);
+            this.Controls.Add(this.pnlContainer);
+            this.Controls.Add(this.BtnNewGrid);
+            this.Controls.Add(this.ComboBoxColor);
+            this.Controls.Add(this.TxtColorChoice);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LabelShop";
+            this.Text = "LabelShop Basique";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TxtColorChoice;
+        private System.Windows.Forms.ComboBox ComboBoxColor;
+        private System.Windows.Forms.Button BtnNewGrid;
+        private System.Windows.Forms.Panel pnlContainer;
+    }
+}
+
---
+++ LabelShop/LabelShop/LabelShop.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LabelShop
+{
+    public partial class LabelShop : Form
+    {
+
+        //by default do not draw
+        private bool isColorSelected = false;
+
+
+        /// <summary>
+        /// Constant : The number of labels in axe x
+        /// </summary>
+        private const int WIDTH = 32;
+
+        /// <summary>
+        /// Constant : The number of labels axe y
+        /// </summary>
+        private const int HEIGHT = 32;
+
+        /// <summary>
+        /// Constant :  The width of the labels
+        /// </summary>
+        private const int LABEL_WIDTH_SIZE = 10;
+
+        /// <summary>
+        /// Constant : The width of the labels
+        /// </summary>
+        private const int LABEL_HEIGHT_SIZE = 10;
+
+
+
+        public LabelShop()
+        {
+            InitializeComponent();
+
+            ComboBoxColor.Items.Add(Color.Green);
+            ComboBoxColor.Items.Add(Color.Red);
+            ComboBoxColor.Items.Add(Color.Blue);
+            ComboBoxColor.Items.Add(Color.Black);
+            ComboBoxColor.Items.Add(Color.White);
+            ComboBoxColor.Items.Add(Color.Yellow);
+            ComboBoxColor.Items.Add(Color.Purple);
+
+            ComboBoxColor.SelectedIndex = 0;
+
+
+
+            
+            for(int i = 0; i < WIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++)
+                {
+                    Label lbl = new Label();
+
+                    lbl.Width = LABEL_WIDTH_SIZE;
+                    lbl.Height = LABEL_HEIGHT_SIZE;
+
+                    lbl.Location = new Point(i* LABEL_WIDTH_SIZE, j* LABEL_HEIGHT_SIZE);
+
+                    lbl.BackColor = Color.White;
+
+                    lbl.MouseMove += new MouseEventHandler(MouseMoveOnLabl);
+                    lbl.MouseClick += new MouseEventHandler(MouseClickOnLabel);
+
+                    pnlContainer.Controls.Add(lbl);
+
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that causes the event</param>
+        /// <param name="e">event-related data</param>
+        private void MouseMoveOnLabl(object sender, EventArgs e)
+        {
+            Label lbl = (Label)sender;
+
+            if (isColorSelected)
+            {
+                lbl.BackColor = (Color)ComboBoxColor.SelectedItem;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MouseClickOnLabel(object sender, EventArgs e)
+        {
+            isColorSelected = !isColorSelected;
+        }
+
+        private void PanelSize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnNewGrid_Click(object sender, EventArgs e)
+        {
+            //browse the table and replace the label by white color
+            foreach(Label lbl in this.pnlContainer.Controls)
+            {
+                lbl.BackColor = Color.White;
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlContainer_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
