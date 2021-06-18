42651f7564d4f3eebe2b502e31a7710f5f6287ac
Anthony Höhn <anthonyhohn@gmail.com>
2021-06-18 14:21:40 +0200

    ex

---
+++ Cavalier/cavalier/WindowsFormsApp1/cavalier.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class cavalier
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.lblCounterClick = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tailleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x8ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x16ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblTxtCount = new System.Windows.Forms.Label();
+            this.pnlContainer = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(41, 470);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(120, 27);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "Nouvelle partie";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.BtnNewGame_Click);
+            // 
+            // lblCounterClick
+            // 
+            this.lblCounterClick.AutoSize = true;
+            this.lblCounterClick.Location = new System.Drawing.Point(400, 470);
+            this.lblCounterClick.Name = "lblCounterClick";
+            this.lblCounterClick.Size = new System.Drawing.Size(13, 13);
+            this.lblCounterClick.TabIndex = 2;
+            this.lblCounterClick.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItem
+            // 
+            this.ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tailleToolStripMenuItem,
+            this.aideToolStripMenuItem});
+            this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+            this.ToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.ToolStripMenuItem.Text = "Options";
+            // 
+            // tailleToolStripMenuItem
+            // 
+            this.tailleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.x8ToolStripMenuItem,
+            this.x16ToolStripMenuItem});
+            this.tailleToolStripMenuItem.Name = "tailleToolStripMenuItem";
+            this.tailleToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.tailleToolStripMenuItem.Text = "Taille";
+            // 
+            // x8ToolStripMenuItem
+            // 
+            this.x8ToolStripMenuItem.Name = "x8ToolStripMenuItem";
+            this.x8ToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.x8ToolStripMenuItem.Text = "8x8";
+            this.x8ToolStripMenuItem.Click += new System.EventHandler(this.X8ToolStripMenuItem_Click);
+            // 
+            // x16ToolStripMenuItem
+            // 
+            this.x16ToolStripMenuItem.Name = "x16ToolStripMenuItem";
+            this.x16ToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.x16ToolStripMenuItem.Text = "16x16";
+            this.x16ToolStripMenuItem.Click += new System.EventHandler(this.X16ToolStripMenuItem_Click);
+            // 
+            // aideToolStripMenuItem
+            // 
+            this.aideToolStripMenuItem.Name = "aideToolStripMenuItem";
+            this.aideToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.aideToolStripMenuItem.Text = "Aide";
+            this.aideToolStripMenuItem.Click += new System.EventHandler(this.AideToolStripMenuItem_Click);
+            // 
+            // lblTxtCount
+            // 
+            this.lblTxtCount.AutoSize = true;
+            this.lblTxtCount.Location = new System.Drawing.Point(303, 470);
+            this.lblTxtCount.Name = "lblTxtCount";
+            this.lblTxtCount.Size = new System.Drawing.Size(91, 13);
+            this.lblTxtCount.TabIndex = 4;
+            this.lblTxtCount.Text = "Nombre de coups";
+            // 
+            // pnlContainer
+            // 
+            this.pnlContainer.BackColor = System.Drawing.Color.White;
+            this.pnlContainer.Location = new System.Drawing.Point(41, 45);
+            this.pnlContainer.Name = "pnlContainer";
+            this.pnlContainer.Size = new System.Drawing.Size(400, 400);
+            this.pnlContainer.TabIndex = 0;
+            // 
+            // cavalier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 521);
+            this.Controls.Add(this.lblTxtCount);
+            this.Controls.Add(this.lblCounterClick);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.pnlContainer);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "cavalier";
+            this.Text = "Cavaliotche";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label lblCounterClick;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tailleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x8ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x16ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aideToolStripMenuItem;
+        private System.Windows.Forms.Label lblTxtCount;
+        private System.Windows.Forms.Panel pnlContainer;
+    }
+}
+
---
+++ Cavalier/cavalier/WindowsFormsApp1/cavalier.cs
@@ -0,0 +1,140 @@
+﻿///ETML
+///Auteur : Anthony Höhn
+///Date : 16.06.2021
+///Description : Cavalier game
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class cavalier : Form
+    {
+        int[,] array = new int[1, 2];
+
+        private int _compteur = 0;
+
+        //private bool _isClicked = true;
+
+        /// The number of button
+        private int _nbrButton = 8;
+
+        /// The width of the labels
+        private int _sizeButton = 50;
+
+        //private Point _position = new Point(100, 100);
+        Control currentLabel;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public cavalier()
+        {
+            InitializeComponent();
+            GenerateGame();
+        }
+
+        /// <summary>
+        /// Grid buttons generations 
+        /// </summary>
+        private void GenerateGame()
+        {
+            lblCounterClick.Text = Convert.ToString(_compteur = 0);
+            pnlContainer.Controls.Clear();
+            //create labels in panel
+            for (int i = 0; i < _nbrButton; i++)
+            {
+                for (int j = 0; j < _nbrButton; j++)
+                {
+                    Button btn = new Button();
+
+                    btn.Width = _sizeButton;
+                    btn.Height = _sizeButton;
+
+                    btn.Location = new Point(i * _sizeButton, j * _sizeButton);
+
+                    btn.MouseClick += new MouseEventHandler(ClickButton);
+
+                    pnlContainer.Controls.Add(btn);
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Set the clicked button in yellow
+        /// </summary>
+        /// <param name="sender">object that causes the event</param>
+        /// <param name="e">event-related data</param>
+        private void ClickButton(object sender, EventArgs e)
+        {
+
+            Point _position = new Point(200, 200);
+            currentLabel = pnlContainer.GetChildAtPoint(_position);
+            currentLabel.BackColor = Color.Yellow;
+
+
+            Button btn = (Button)sender;
+
+            btn.BackColor = Color.Red;
+            lblCounterClick.Text = Convert.ToString(++_compteur);
+        }
+
+        /// <summary>
+        /// Clear the buttons and sets the number of moves to 0
+        /// </summary>
+        /// <param name="sender">object that causes the event</param>
+        /// <param name="e">event-related data</param>
+        private void BtnNewGame_Click(object sender, EventArgs e)
+        {
+            foreach (Button btn in pnlContainer.Controls)
+            {
+                btn.BackColor = Color.White;
+            }
+
+            lblCounterClick.Text = Convert.ToString(_compteur = 0);
+        }
+
+        /// <summary>
+        /// Generate the game in 8x8
+        /// </summary>
+        /// <param name="sender">object that causes the event</param>
+        /// <param name="e">event-related data</param>
+        private void X8ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _sizeButton = 50;
+            _nbrButton = 8;
+            GenerateGame();
+        }
+
+        /// <summary>
+        /// Generate the game in 16x16
+        /// </summary>
+        /// <param name="sender">object that causes the event</param>
+        /// <param name="e">event-related data</param>
+        private void X16ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _sizeButton = 25;
+            _nbrButton = 16;
+            GenerateGame();
+        }
+
+        /// <summary>
+        /// Pop-up text d'aide
+        /// </summary>
+        /// <param name="sender">object that causes the event</param>
+        /// <param name="e">event-related data</param>
+        private void AideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Voici l'aide ! LoL");
+        }
+
+        private void TextBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                MessageBox.Show("test");    
+            }
+        }
+    }
+}
